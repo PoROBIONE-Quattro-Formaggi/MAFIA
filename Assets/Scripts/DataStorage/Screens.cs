eaecebd3989b5df1c151ae5e2983cf1e6afb2d66
Weronika <wera.jedras@gmail.com>
2023-11-04 02:40:04 +0100

    some screens

--- Assets/Scripts/DataStorage/Screens.cs
+++ Assets/Scripts/DataStorage/Screens.cs
@@ -3,9 +3,9 @@ namespace DataStorage
     public static class Screens
     {
         public const string JoinLobby = "JoinLobby";
-        public const string MainScreen = "MainScreen";
+        public const string MainScreen = "Main";
         public const string CreateLobbyScreen = "CreateLobbyScreen";
-        public const string EnterCodeScreen = "EnterCodeScreen";
+        public const string EnterCodeScreen = "EnterCode";
         public const string SetNameScreen = "SetNameScreen";
         public const string LobbyHostScreen = "LobbyHostScreen";
         public const string LobbyPlayerScreen = "LobbyPlayerScreen";
