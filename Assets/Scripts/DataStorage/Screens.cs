e324fd3e4c3419e547d85bd901e2599c55f189db
Weronika <wera.jedras@gmail.com>
2023-11-05 19:25:46 +0100

    screen rework done

---
+++ Assets/Scripts/DataStorage/Screens.cs
@@ -0,0 +1,13 @@
+namespace DataStorage
+{
+    public static class Screens
+    {
+        public const string JoinLobby = "JoinLobby";
+        public const string MainScreen = "MainScreen";
+        public const string CreateLobbyScreen = "CreateLobbyScreen";
+        public const string EnterCodeScreen = "EnterCodeScreen";
+        public const string SetNameScreen = "SetNameScreen";
+        public const string LobbyHostScreen = "LobbyHostScreen";
+        public const string LobbyPlayerScreen = "LobbyPlayerScreen";
+    }
+}
