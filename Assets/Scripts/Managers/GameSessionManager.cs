4ec8b0d3c377aa988c084d7f8cb0ed0bfa1ae6f4
Kacper Radota <72502652+KacperRadota@users.noreply.github.com>
2023-11-13 12:58:01 +0100

    First try of role assignment

--- Assets/Scripts/Managers/GameSessionManager.cs
+++ Assets/Scripts/Managers/GameSessionManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using DataStorage;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +8,10 @@ namespace Managers
 {
     public class GameSessionManager : MonoBehaviour
     {
+        public int mafiaNumber = 1;
+        public int doctorNumber = 1;
+        public int residentNumber = 2;
+
         public static GameSessionManager Instance
         {
             get
@@ -18,6 +24,9 @@ namespace Managers
                 return _instance;
             }
         }
+
+        public Dictionary<ulong, string> IdxRole { get; } = new();
+        public List<ulong> ClientsIds { get; private set; } = new();
 
         private static GameSessionManager _instance;
 
@@ -40,7 +49,7 @@ namespace Managers
             SceneManager.sceneLoaded += OnSceneChanged;
         }
 
-        private static void OnSceneChanged(Scene scene, LoadSceneMode loadSceneMode)
+        private void OnSceneChanged(Scene scene, LoadSceneMode loadSceneMode)
         {
             if (SceneManager.GetActiveScene().name != Scenes.GameScene) return;
             var joinCode = PlayerPrefs.GetString(PpKeys.KeyStartGame);
@@ -52,12 +61,84 @@ namespace Managers
             else if (isHost == 1)
             {
                 RelayManager.Instance.CreateRelay(); // Here NetworkManager automatically connects as a Host I think
-                // NetworkCommunicationManager.StartHost();
+                Debug.Log("Invoking Assigning players");
+                InvokeRepeating(nameof(TryToAssignPlayersToRoles), 0f, 0.1f);
             }
             else
             {
                 RelayManager.JoinRelay(joinCode);
-                // NetworkCommunicationManager.StartClient(); TODO chyba to wyłączone IDK
+            }
+        }
+
+        private void TryToAssignPlayersToRoles()
+        {
+            Debug.Log($"Network spawned: {NetworkCommunicationManager.Instance.IsNetworkSpawned}");
+            if (!NetworkCommunicationManager.Instance.IsNetworkSpawned) return;
+            var hostID = NetworkCommunicationManager.Instance.OwnerClientId;
+            var playersIDs = NetworkCommunicationManager.GetAllConnectedPlayersIDs();
+            foreach (var id in playersIDs.Where(id => id == hostID))
+            {
+                playersIDs.Remove(id);
+            }
+
+            ClientsIds = playersIDs;
+            Shuffle(playersIDs);
+            var rolesList = GetRolesList(playersIDs.Count);
+
+            for (var i = 0; i < rolesList.Count; i++)
+            {
+                IdxRole[playersIDs[i]] = rolesList[i];
+            }
+
+            IdxRole[hostID] = Roles.Narrator;
+            Debug.Log("Assigned players");
+            CancelInvoke(nameof(TryToAssignPlayersToRoles));
+        }
+
+        private List<string> GetRolesList(int playerNumber)
+        {
+            List<string> rolesList = new();
+            foreach (var field in typeof(Roles).GetFields())
+            {
+                var roleName = field.GetValue(typeof(Roles));
+                switch (roleName)
+                {
+                    case Roles.Mafia:
+                        for (var i = 0; i < mafiaNumber; i++)
+                        {
+                            rolesList.Add(Roles.Mafia);
+                        }
+
+                        break;
+                    case Roles.Doctor:
+                        for (var i = 0; i < doctorNumber; i++)
+                        {
+                            rolesList.Add(Roles.Doctor);
+                        }
+
+                        break;
+                    case Roles.Resident:
+                        for (var i = 0; i < playerNumber - doctorNumber - mafiaNumber; i++)
+                        {
+                            rolesList.Add(Roles.Resident);
+                        }
+
+                        break;
+                }
+            }
+
+            return rolesList;
+        }
+
+        private static void Shuffle(IList<ulong> listToShuffle)
+        {
+            var rng = new System.Random();
+            var n = listToShuffle.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                (listToShuffle[k], listToShuffle[n]) = (listToShuffle[n], listToShuffle[k]);
             }
         }
 
--- Assets/Scripts/UI/KeyboardController.cs
+++ Assets/Scripts/UI/KeyboardController.cs
@@ -1,112 +1,103 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using UI;
 
-public class KeyboardController : MonoBehaviour
+namespace UI
 {
-    public RectTransform screenRect;
-    public TMP_InputField inputField;
-    
-    [Header("Variables")]
-    public bool caps;
-
-    public float heightOnScreen;
-
-    public float spacingRatio;
-    
-    
-    private RectTransform _keyboardTransform;
-    private List<HorizontalLayoutGroup> _rows = new List<HorizontalLayoutGroup>();
-    private List<TextMeshProUGUI> _chars = new List<TextMeshProUGUI>();
-
-    
-
-    private void Start()
+    public class KeyboardController : MonoBehaviour
     {
-        _keyboardTransform = GetComponent<RectTransform>();
-        
-        connectKeys();
-        
-        foreach (var row in _keyboardTransform.GetComponentsInChildren<HorizontalLayoutGroup>())
+        public RectTransform screenRect;
+        public TMP_InputField inputField;
+
+        [Header("Variables")] public bool caps;
+
+        public float heightOnScreen;
+        public float spacingRatio;
+
+        private RectTransform _keyboardTransform;
+        private readonly List<HorizontalLayoutGroup> _rows = new();
+        private readonly List<TextMeshProUGUI> _chars = new();
+
+        private void Start()
         {
-            _rows.Add(row);
-        }
-        
-        foreach (var keyChar in GetComponentsInChildren<TextMeshProUGUI>())
-        {
-            _chars.Add(keyChar);
-            if (caps)
+            _keyboardTransform = GetComponent<RectTransform>();
+            ConnectKeys();
+            foreach (var row in _keyboardTransform.GetComponentsInChildren<HorizontalLayoutGroup>())
             {
-                keyChar.text = keyChar.text.ToUpper();
+                _rows.Add(row);
+            }
+
+            foreach (var keyChar in GetComponentsInChildren<TextMeshProUGUI>())
+            {
+                _chars.Add(keyChar);
+                if (caps)
+                {
+                    keyChar.text = keyChar.text.ToUpper();
+                }
             }
         }
-    }
 
-    private void Update()
-    {
-        for (int i = 0; i < _rows.Count; i++)
+        private void Update()
         {
-            if (i == 4)
+            for (var i = 0; i < _rows.Count; i++)
             {
-                _rows[i].spacing = screenRect.sizeDelta.x / (spacingRatio / 10);
+                if (i == 4)
+                {
+                    _rows[i].spacing = screenRect.sizeDelta.x / (spacingRatio / 10);
+                }
+                else
+                {
+                    _rows[i].spacing = screenRect.sizeDelta.x / spacingRatio;
+                }
+            }
+        }
+
+        public void ConnectKeys()
+        {
+            // Connect key scripts with input field
+            foreach (var keyScript in _keyboardTransform.GetComponentsInChildren<InputChar>())
+            {
+                keyScript.inputField = inputField;
+            }
+
+            // Connect delete with input field
+            _keyboardTransform.GetComponentInChildren<DeleteKey>().inputField = inputField;
+
+            // Connect enter with input field
+            _keyboardTransform.GetComponentInChildren<EnterKey>().inputField = inputField;
+        }
+
+        public void ShowKeyboard()
+        {
+            //if(!Application.isMobilePlatform) return;
+            _keyboardTransform.anchoredPosition = new Vector2(0, _keyboardTransform.sizeDelta.y + heightOnScreen);
+        }
+
+        public void HideKeyboard()
+        {
+            //if(!Application.isMobilePlatform) return;
+            _keyboardTransform.anchoredPosition = new Vector2(0, 0);
+        }
+
+        public void OnCapsPressed()
+        {
+            // toggle caps
+            caps = !caps;
+
+            if (caps)
+            {
+                foreach (var keyChar in _chars)
+                {
+                    keyChar.text = keyChar.text.ToUpper();
+                }
             }
             else
             {
-                _rows[i].spacing = screenRect.sizeDelta.x / spacingRatio;
-            }
-        }
-    }
-
-    public void connectKeys()
-    {
-        // Connect key scripts with input field
-        foreach (var keyScript in _keyboardTransform.GetComponentsInChildren<InputChar>())
-        {
-            keyScript.inputField = inputField;
-        }
-        
-        // Connect delete with input field
-        _keyboardTransform.GetComponentInChildren<DeleteKey>().inputField = inputField;
-
-        // Connect enter with input field
-        _keyboardTransform.GetComponentInChildren<EnterKey>().inputField = inputField;
-    }
-
-    public void ShowKeyboard()
-    {
-        //if(!Application.isMobilePlatform) return;
-        _keyboardTransform.anchoredPosition = new Vector2(0, _keyboardTransform.sizeDelta.y + heightOnScreen);
-    }
-
-    public void HideKeyboard()
-    {
-        //if(!Application.isMobilePlatform) return;
-        _keyboardTransform.anchoredPosition = new Vector2(0, 0);
-    }
-    
-    public void OnCapsPressed()
-    {
-        // toggle caps
-        caps = !caps;
-            
-        if (caps)
-        {
-            foreach (var keyChar in _chars)
-            {
-                
-                keyChar.text = keyChar.text.ToUpper();
-            }
-        }
-        else
-        {
-            foreach (var keyChar in _chars)
-            {
-                keyChar.text = keyChar.text.ToLower();
+                foreach (var keyChar in _chars)
+                {
+                    keyChar.text = keyChar.text.ToLower();
+                }
             }
         }
     }
